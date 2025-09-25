d91e1b0ab17e4f9fa157be93250ed5173200ec7a
Adam Campbell <adamcampbelldev@gmail.com>
2025-09-26 00:17:10 +0100

    add styling and conditional messages to error page

--- frontend/src/pages/Error.tsx
+++ frontend/src/pages/Error.tsx
@@ -1,31 +1,55 @@
-import { useRouteError, isRouteErrorResponse } from "react-router";
+import { useRouteError, isRouteErrorResponse, Route } from "react-router";
+import { Typography, Container, Button } from "@mui/material";
+import { Link as RouterLink } from "react-router";
 
 function getMessage(status: number): string {
     const statusMessageMap: Record<number, string> = {
-        401: "You must sign in before continuing",
-        403: "You are not permitted to access this resource",
-        404: "The requested resource could not be found",
+        400: "The page address is invalid. Please check the link and try again.",
+        401: "You must sign in before continuing.",
+        403: "You are not permitted to access this resource.",
+        404: "The requested resource could not be found.",
+        500: "Something went wrong on our end. Please try again later.",
     };
 
-    return statusMessageMap[status] ?? "Fallback goes here";
+    return statusMessageMap[status] ?? "An unknown error occurred.";
 }
 
 export default function ErrorPage() {
     const error = useRouteError();
 
-    if (isRouteErrorResponse(error)) {
-        return (
-            <>
-                <h1>{error.status}</h1>
-                <p>{getMessage(error.status)}</p>
-            </>
-        );
-    } else {
-        return (
-            <>
-                <h1>An unexpected error has occurred</h1>
-                <p>DON'T PANIC DON'T PANIC</p>
-            </>
-        );
-    }
+    const errorData = isRouteErrorResponse(error)
+        ? { status: error.status, message: getMessage(error.status) }
+        : { status: "Error", message: "An unknown error occurred." };
+
+    return (
+        <Container
+            maxWidth="lg"
+            sx={{
+                minHeight: ["100dvh", "100vh"],
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                textAlign: "center",
+            }}
+        >
+            <Typography variant="h1">{errorData.status}</Typography>
+            <Typography variant="subtitle1" sx={{ mb: 2 }}>
+                {errorData.message}
+            </Typography>
+            {errorData.status === 401 ? (
+                <Button
+                    variant="contained"
+                    component={RouterLink}
+                    to="/sign-in"
+                >
+                    Sign In
+                </Button>
+            ) : (
+                <Button variant="contained" component={RouterLink} to="/">
+                    Return Home
+                </Button>
+            )}
+        </Container>
+    );
 }
